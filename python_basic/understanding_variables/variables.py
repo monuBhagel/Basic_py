413ec058397918b3cb35965a1feb6e9bfdea02ad
monu <monubhagel90@gmail.com>
2025-03-08 21:49:50 +0530

    add variables.py

---
+++ python_basic/understanding_variables/variables.py
@@ -0,0 +1,18 @@
+# Generally , variables are used to store data values in a program . but in python , variables are actually the references to the objects in the memory 
+
+# variables act as the "placeholder" for the data . they allow us to store and manipulate data in a program .
+
+
+
+# Creating a variable in python 
+
+'''variable_name = value/data'''
+
+num = 5 
+
+double_qoutes_name = "python"
+
+single_quotes_name = 'python3'
+
+
+
