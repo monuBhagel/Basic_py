598394c4c87e58c511e4c4b8211d525eb44cb1fe
monu <monubhagel90@gmail.com>
2025-03-10 22:25:35 +0530

    add output.py in I/O

---
+++ Input_output/output.py
@@ -0,0 +1,8 @@
+# Printing Output using print() in Python
+
+# the print() function allow us to display text, variables and expressions 
+# on the console
+
+print("hello world")
+
+print('hi everyone')
