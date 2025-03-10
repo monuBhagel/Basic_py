a8396047d91fc349fcf07399e11992b57cb4a874
monu <monubhagel90@gmail.com>
2025-03-10 22:25:58 +0530

    add printing_var.py in I/O

---
+++ Input_output/printing_var.py
@@ -0,0 +1,11 @@
+# We can print multiple variables by separating them with commas.
+
+# Single variable
+name = "bob"
+print(name)
+
+# Multiple Variables
+name = "sonu"
+age = 25
+city = "delhi"
+print(name, age, city)
