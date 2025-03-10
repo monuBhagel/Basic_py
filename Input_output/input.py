fdc3ad6fbbc60df085a4036d5ab9074528f4f40c
monu <monubhagel90@gmail.com>
2025-03-10 22:24:50 +0530

    add input.py in I/O

---
+++ Input_output/input.py
@@ -0,0 +1,11 @@
+# there are scenerios where we need to take input from the user insted of 
+# hard-codeing the value . for this specific scene python provides us the
+
+# built-in function known as "input()"
+
+# Taking input in Python
+
+'''Python input() function is used to take user input.
+By default, it returns the user input in form of a string. '''
+
+name = input("enter you name : ")
